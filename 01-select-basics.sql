694b48d0b589797599acf3c9279e620eba39b2f8
Ayman-Cyber-Security <cybersecuritystudent7@gmail.com>
2026-07-17 19:41:01 +0300

    Create 01-select-basics.sql
    ثلاثه تمارين على اول اساس select 
     طريقة استدعاء الجدول كاملا 
    وتحديد صفوف محدده 
    واعادة نعريف اسم الصف في شاشة المخرجات

---
+++ 01-select-basics.sql
@@ -0,0 +1,4 @@
+SELECT *FROM employees;-- select all the elements from the table
+select first_name,last_name,salary from employees;-- select specifc column from the table
+
+select first_name,last_name,salary as "Monthly Salary" from employees;--Rename column  
